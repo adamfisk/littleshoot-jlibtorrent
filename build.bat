6a2a7eb07be09be8005e6454fa70da5491651c88
jn <j@littleshoot.org>
2009-01-07 05:40:51 +0000

    :TODO: add include paths for boost and friends.
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/jni/jlibtorrent@4878 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
 javac ./src/main/java/org/lastbamboo/jni/JLibTorrent.java
 javah -jni JLibTorrent
 
+REM :TODO: add include paths for boost and friends.
+
 cl -I"c:\program files\java\jdk1.6.0_11\include" -I"c:\program files\java\jdk1.6.0_11\include\win32" -I"." -MT -LD ./src/main/cpp/jnltorrentjnilib.cpp -Fejnilibtorrent.dll  
