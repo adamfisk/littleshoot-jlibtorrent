f1484871160f55afff93e169ca67d7acd4f85b43
jn <j@littleshoot.org>
2009-01-07 05:39:01 +0000

    JNI Support for win32 and win64.
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/jni/jlibtorrent@4877 8d7aff80-242d-0410-bcee-ed6f15385b3d

---
+++ build.bat
@@ -0,0 +1,4 @@
+javac ./src/main/java/org/lastbamboo/jni/JLibTorrent.java
+javah -jni JLibTorrent
+
+cl -I"c:\program files\java\jdk1.6.0_11\include" -I"c:\program files\java\jdk1.6.0_11\include\win32" -I"." -MT -LD ./src/main/cpp/jnltorrentjnilib.cpp -Fejnilibtorrent.dll  
