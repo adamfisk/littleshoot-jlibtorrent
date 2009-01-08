ae7005c534f5695cd764a56337cf9a40642ee7a0
adamfisk <a@littleshoot.org>
2009-01-08 04:59:55 +0000

    win tweakings
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/jni/jlibtorrent@4900 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- compile.bat
+++ compile.bat
@@ -1,2 +1,2 @@
 
-cl -I "c:\program files\java\jdk1.6.0_11\include" -I"c:\program files\java\jdk1.6.0_11\include\win32" -I"c:\boost" -I "../../libtorrent/libtorrent/include" -I "../../libtorrent/libtorrent/zlib" -I "./build/headers" -I"." /LD /MT /EHsc /W1  /TP ./src/main/cpp/jnltorrentjnilib.cpp /Fe"jlibtorrent.dll"
+cl -I "c:\program files\java\jdk1.6.0_11\include" -I"c:\program files\java\jdk1.6.0_11\include\win32" -I"c:\boost" -I "../../libtorrent/libtorrent/include" -I "../../libtorrent/libtorrent/zlib" -I "./build/Release/Headers" -I"." /LD /MT /EHsc /W1  /TP ./src/main/cpp/jnltorrentjnilib.cpp /Fe"jlibtorrent.dll"
