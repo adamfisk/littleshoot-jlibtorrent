ff07463d291afb6a4960c583a6a8bb6d2b935d17
jn <j@littleshoot.org>
2009-01-08 20:25:31 +0000

    Now builds Debug JNI dll.
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/jni/jlibtorrent@4915 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- build.bat
+++ build.bat
@@ -3,4 +3,4 @@ javac ./src/main/java/org/lastbamboo/jni/JLibTorrent.java
 
 javah -verbose -classpath ./src/main/java/ -force -d build/headers/ org.lastbamboo.jni.JLibTorrent
 
-cl -I "c:\program files\java\jdk1.6.0_11\include" -I"c:\program files\java\jdk1.6.0_11\include\win32" -I"c:\boost" -I "../../libtorrent/libtorrent/include" -I "../../libtorrent/libtorrent/zlib" -I "./build/headers" -I"." /LD /MT /EHsc /W1  /TP ./src/main/cpp/jnltorrentjnilib.cpp /Fe"jlibtorrent.dll"
+devenv jlibtorrent.sln jlibtorrent.vcproj /build Debug
