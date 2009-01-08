a637924b31d005f5b0d57305e9d69b00c3e1543e
jn <j@littleshoot.org>
2009-01-08 04:18:34 +0000

    Building works, linking does not.
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/jni/jlibtorrent@4897 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- build.bat
+++ build.bat
@@ -1,9 +1,6 @@
 
-REM FIXME: There is a bug in class generation with: javah -jni JLibTorrent
-
 javac ./src/main/java/org/lastbamboo/jni/JLibTorrent.java
 
-javah -jni JLibTorrent
+javah -verbose -classpath ./src/main/java/ -force -d build/headers/ org.lastbamboo.jni.JLibTorrent
 
-
-cl /Od /D "WIN32" /D "_DEBUG" /D "_USRDLL" /D "_WINDLL" -I"c:\program files\java\jdk1.6.0_11\include" -I"c:\program files\java\jdk1.6.0_11\include\win32" -I"c:\boost" -I "../../libtorrent/libtorrent/include" -I "../../libtorrent/libtorrent/zlib" -I"." -LD /Gm /EHsc /RTC1 /MDd /Fo"Debug\\" /W3 /nologo /c /ZI /TP /errorReport:prompt ./src/main/cpp/jnltorrentjnilib.cpp -Fejnilibtorrent.dll  
+cl -I "c:\program files\java\jdk1.6.0_11\include" -I"c:\program files\java\jdk1.6.0_11\include\win32" -I"c:\boost" -I "../../libtorrent/libtorrent/include" -I "../../libtorrent/libtorrent/zlib" -I "./build/headers" -I"." /LD /MT /EHsc /W1  /TP ./src/main/cpp/jnltorrentjnilib.cpp /Fe"jlibtorrent.dll"
