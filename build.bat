dc7dd851db40f869cf1186202779d4142e28cb30
jn <j@littleshoot.org>
2009-02-17 02:14:13 +0000

    Cleanups
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/jni/jlibtorrent@5624 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- build.bat
+++ build.bat
@@ -3,4 +3,4 @@ javac ./src/main/java/org/lastbamboo/jni/JLibTorrent.java
 
 javah -verbose -classpath ./src/main/java/ -force -d build/headers/ org.lastbamboo.jni.JLibTorrent
 
-devenv jlibtorrent.sln jlibtorrent.vcproj /build Debug
+devenv jlibtorrent.vcproj/jlibtorrent.sln jlibtorrent.vcproj/jlibtorrent.vcproj /build Debug
