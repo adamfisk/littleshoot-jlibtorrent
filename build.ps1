45edbfbe49fc70608941221d23f3c37a35991a17
adamfisk <a@littleshoot.org>
2009-02-19 05:55:57 +0000

    Added ps build and copying of dll to lib dir
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/jni/jlibtorrent@5640 8d7aff80-242d-0410-bcee-ed6f15385b3d

---
+++ build.ps1
@@ -0,0 +1,7 @@
+javac ./src/main/java/org/lastbamboo/jni/JLibTorrent.java
+
+javah -verbose -classpath ./src/main/java/ -force -d build/headers/ org.lastbamboo.jni.JLibTorrent
+
+devenv jlibtorrent.vcproj/jlibtorrent.sln jlibtorrent.vcproj/jlibtorrent.vcproj /build Debug
+
+cp ./jlibtorrent.vcproj/Debug/jlibtorrent.dll ../../lib
