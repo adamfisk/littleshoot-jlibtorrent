4fdc29cce7584d5145bbba884dd184dfd420c370
jn <j@littleshoot.org>
2009-02-22 01:57:14 +0000

    linker fixers for release builds
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/jni/jlibtorrent@5669 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- build.bat
+++ build.bat
@@ -4,3 +4,5 @@ javac ./src/main/java/org/lastbamboo/jni/JLibTorrent.java
 javah -verbose -classpath ./src/main/java/ -force -d build/headers/ org.lastbamboo.jni.JLibTorrent
 
 devenv jlibtorrent.vcproj/jlibtorrent.sln jlibtorrent.vcproj/jlibtorrent.vcproj /build Debug
+
+devenv jlibtorrent.vcproj/jlibtorrent.sln jlibtorrent.vcproj/jlibtorrent.vcproj /build Release
