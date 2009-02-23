069295e626d453ecd432cae9683a330bdff9bca3
adamfisk <a@littleshoot.org>
2009-02-23 00:25:30 +0000

    added error checking
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/jni/jlibtorrent@5683 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- buildBase.ps1
+++ buildBase.ps1
@@ -1,9 +1,20 @@
 javac ./src/main/java/org/lastbamboo/jni/JLibTorrent.java
 
+if (!$?)
+{
+    Write-Output "Could not build Java file.  Exiting" 
+    exit 1
+}
+
 javah -verbose -classpath ./src/main/java/ -force -d build/headers/ org.lastbamboo.jni.JLibTorrent
 
 $buildArg = $args[0]
 $buildConfig = $args[1]
 devenv jlibtorrent.vcproj/jlibtorrent.sln jlibtorrent.vcproj/jlibtorrent.vcproj $buildArg $buildConfig
+if (!$?)
+{
+    Write-Output "Could not build JNI project for LibTorrent. Exiting" 
+    exit 1
+}
 
 cp ./jlibtorrent.vcproj/Debug/jlibtorrent.dll ../../lib/jnltorrent.dll
