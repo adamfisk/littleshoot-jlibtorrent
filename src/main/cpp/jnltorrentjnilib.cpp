92d0ca9c3648253c7aa9e18b3e1e35f48a5cbcbc
jn <j@littleshoot.org>
2009-03-16 07:51:09 +0000

    Fixed memory leak.
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/jni/jlibtorrent@6146 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- src/main/cpp/jnltorrentjnilib.cpp
+++ src/main/cpp/jnltorrentjnilib.cpp
@@ -242,6 +242,20 @@ class session : private boost::noncopyable
 
             return handle;
 		}
+        
+        void remove_torrent(const char * torrent_path)
+        {
+            if (torrent_path)
+            {
+                const libtorrent::torrent_handle th = session::instance(
+                    ).handle(torrent_path)
+                ;
+                
+                session::instance().get_session()->remove_torrent(th);
+                
+                m_torrent_path_to_handle.erase(torrent_path);
+            }
+        }
 	
         const libtorrent::torrent_handle handle(const char* torrentPath)
         {
@@ -853,12 +867,11 @@ JNIEXPORT jlong JNICALL Java_org_lastbamboo_jni_JLibTorrent_get_1size_1for_1torr
 ) {return longCall(env, arg, &sizeFunc);}
 
 
-void removeFunc(const char* torrentPath)
+void removeFunc(const char * torrentPath)
 {    
-    const libtorrent::torrent_handle th = session::instance().handle(torrentPath);
-    if (th.is_valid())
+    if (torrentPath)
     {
-        session::instance().get_session()->remove_torrent(th);
+        session::instance().remove_torrent(torrentPath);
     }
 }    
 JNIEXPORT void JNICALL Java_org_lastbamboo_jni_JLibTorrent_remove_1torrent(
