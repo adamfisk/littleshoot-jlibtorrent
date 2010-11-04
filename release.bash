41e446ee6dee878250e1aeb680f666ca4f7f1401
adamfisk <a@littleshoot.org>
2010-11-04 06:07:53 +0000

    more error catching
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/jni/jlibtorrent@7430 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- release.bash
+++ release.bash
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-mvn install -Dmaven.test.skip=true
-cd ../../lib
-tar czvf jnl.tgz libjnltorrent.jnilib
-aws -putp littleshoot jnl.tgz
+function die()
+{
+  echo $*
+  exit 1
+}
+
+mvn install -Dmaven.test.skip=true || die "Could not build JNI lib"
+cd ../../lib || die "Could not cd"
+rm jnl.tgz 
+tar czvf jnl.tgz libjnltorrent.jnilib || die "Could not tgz"
+aws -putp littleshoot jnl.tgz || die "Could no upload tgz"
 
 cd -
